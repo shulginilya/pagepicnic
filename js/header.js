8cfa3cb30d36565f1479b3ae82790b9da60dddeb
Shulgin Ilya <ishulgin8@gmail.com>
2014-10-13 19:02:02 +0400

    ongoing updates

--- js/header.js
+++ js/header.js
@@ -10,3 +10,35 @@ function moreNavigationActivator(e) {
 		$('.top_under_header').addClass('act');
 	}
 }
+
+function animated_scroll_top() {
+	$("html, body").stop().animate(
+		{
+			scrollTop: 0
+		}, 500, function() {
+			
+		}
+	)
+}
+
+function activateMobileMenu(e) {
+	$('.header').removeClass('fixed');
+	if($("#root").hasClass('m_toggle')) {
+		// $("#root").animate({
+  //      left: '0px'
+  //   }, { duration: 200, queue: false });
+  //   $(".header").animate({
+  //      left: '0px'
+  //   }, { duration: 200, queue: false });
+		$("#root, .mobile_sidebar_menu, .header, body").removeClass('m_toggle');
+	} else {
+		// $("#root").animate({
+  //      left: '-260px'
+  //   }, { queue: false });
+  //   $(".header").animate({
+  //      left: '-260px'
+  //   }, { queue: false });
+		$("#root, .mobile_sidebar_menu, .header, body").addClass('m_toggle');
+	}
+	animated_scroll_top();
+}
--- js/screen.js
+++ js/screen.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
 
-	function animated_scroll_top() {
-		$("html, body").stop().animate(
-			{
-				scrollTop: 0
-			}, 500, function() {
-				
-			}
-		)
-	}
-
 	$(window).scroll(function() {
 		$(".mobile_sidebar_menu").height($(document).height() + "px");
 		if(!$(".mobile_sidebar_menu").hasClass('m_toggle')) {
@@ -30,27 +20,5 @@ $(document).ready(function() {
 	// 	}
 	// 	animated_scroll_top();
 	// });
-
-	$(".mob_nav_knob > a").bind("click", function(e) {
-		$('.header').removeClass('fixed');
-		if($("#root").hasClass('m_toggle')) {
-			// $("#root").animate({
-	  //      left: '0px'
-	  //   }, { duration: 200, queue: false });
-	  //   $(".header").animate({
-	  //      left: '0px'
-	  //   }, { duration: 200, queue: false });
-			$("#root, .mobile_sidebar_menu, .header, body").removeClass('m_toggle');
-		} else {
-			// $("#root").animate({
-	  //      left: '-260px'
-	  //   }, { queue: false });
-	  //   $(".header").animate({
-	  //      left: '-260px'
-	  //   }, { queue: false });
-			$("#root, .mobile_sidebar_menu, .header, body").addClass('m_toggle');
-		}
-		animated_scroll_top();
-	});
 
 });
