5b430435dbea17776edcc82235385ba2acce15d2
Shulgin Ilya <ishulgin8@gmail.com>
2014-10-13 19:12:18 +0400

    ongoing updates

---
+++ js/mobile_sidebar_menu.js
@@ -0,0 +1,4 @@
+$(function(){
+  	$("#mobile_sidebar_menu").load("/partials/mobile_sidebar_menu.html"); // === for local
+  	// $("#mobile_sidebar_menu").load("/pagepicnic/partials/mobile_sidebar_menu.html"); // === for net-faces
+});
