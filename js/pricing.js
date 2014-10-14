34b1f61172d13f1eff820e65e405bd81a9d49a52
Shulgin Ilya <ishulgin8@gmail.com>
2014-10-14 17:56:56 +0400

    ongoing updates

--- js/pricing.js
+++ js/pricing.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$('.info_icon').tooltip({
 		html: true
 	});
+
+	$(".all_f_dd_lnk").bind("click", function(e) {
+		$.scrollTo(".all_features_section", 500);
+	});
 });
