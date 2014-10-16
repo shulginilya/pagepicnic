fad061d8ca8cd93fbb0b7dbc73abf073ee0395ad
Shulgin Ilya <ishulgin8@gmail.com>
2014-10-16 15:39:43 +0400

    ongoing updates

--- js/header.js
+++ js/header.js
@@ -24,21 +24,8 @@ function animated_scroll_top() {
 function activateMobileMenu(e) {
 	$('.header').removeClass('fixed');
 	if($("#root").hasClass('m_toggle')) {
-		// $("#root").animate({
-  //      left: '0px'
-  //   }, { duration: 200, queue: false });
-  //   $(".header").animate({
-  //      left: '0px'
-  //   }, { duration: 200, queue: false });
 		$("#root, .mobile_sidebar_menu, .header, body").removeClass('m_toggle');
 	} else {
-		// $("#root").animate({
-  //      left: '-260px'
-  //   }, { queue: false });
-  //   $(".header").animate({
-  //      left: '-260px'
-  //   }, { queue: false });
 		$("#root, .mobile_sidebar_menu, .header, body").addClass('m_toggle');
 	}
-	animated_scroll_top();
 }
